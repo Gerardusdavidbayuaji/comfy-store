cc5ffb919e190e3103c26b1fe066aaf14a5beb2c
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-08-29 02:48:13 +0700

    feat: add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
-import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
 
 const App = () => {
   return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
