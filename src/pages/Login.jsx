a98b4bf2fb6a21dd40f05910ec3844605e4dfc3f
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-09-02 22:08:51 +0700

    add: form register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { FormInput, SubmitBtn } from "../components";
+import { Form, Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid place-items-center">
+      <form
+        method="POST"
+        className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+      >
+        <h4 className="text-center text-3xl font-bold">Login</h4>
+        <FormInput
+          type="email"
+          label="Email"
+          name="identifier"
+          defaultValue="test@gmail.com"
+        />
+        <FormInput
+          type="password"
+          label="Password"
+          name="password"
+          defaultValue="secret"
+        />
+        <div className="mt-4">
+          <SubmitBtn text="login" />
+        </div>
+        <button type="button" className="btn btn-secondary btn-block">
+          Guest User
+        </button>
+        <p className="text-center">
+          Not a member yet?
+          <Link
+            to="/register"
+            className="ml-2 link link-hover link-primary capitalize"
+          >
+            Register
+          </Link>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
