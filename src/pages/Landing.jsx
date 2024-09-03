5799854dfb57babc30284db7bae1c37463a8db39
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-09-03 13:42:21 +0700

    feat: add landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Hero from "../components/Hero";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Landing;
